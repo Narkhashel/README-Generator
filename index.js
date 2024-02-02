b58c4325d190636840e069fd9ce071f581b9c79f
Pawel <pochmarek@gmail.com>
2024-02-02 20:29:37 +0000

    Adding index.js

---
+++ index.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+function generateQuestions() {
+const questions = [
+        {
+            type: 'input',
+            message: 'Name of the Project:',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Short Description of you Project:',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Instalation intructions:',
+            name: 'instalation'
+        },
+        {
+            type: 'input',
+            message: 'Usage information:',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'Contribution guidelines:',
+            name: 'contribution'
+        },
+        {
+            type: 'input',
+            message: 'Test instructions',
+            name: 'test'
+        },
+        {
+            type: 'list',
+            message: 'Please chose your License.',
+            choices: ['MIT','Academic Free License v3.0','European Union Public License 1.1','Microsoft Public License','The Unlicense','Open Software License 3.0'],
+            name: 'license'        
+        },
+        {
+            type: 'input',
+            message: 'GitHub username:',
+            name: 'gitHub'
+        },
+        {
+            type: 'input',
+            message: 'User Email address:',
+            name: 'email'
+        }
+    ];
+    return questions
+}
+
+// function to prompt questions 
+async function promptUser(questions) {
+    let answers = {};
+    answers = await inquirer.prompt(questions);
+    console.log(answers);
+    return answers;
+}
+
+// function to write README file
+async function writeToFile(fileName, data) {
+    try {
+        await fs.writeFile(fileName, data);
+    }
+    catch (err) {
+        console.log("Error writing to file", err);
+    }
+}
+
+// function to initialize program
+async function init() {
+    const questions = generateQuestions();
+    const answers = await promptUser(questions);
+    const readmeMd = generateMarkdown(answers);
+    writeToFile("README.md", readmeMd);
+}
+
+// function call to initialize program
+init();
